bb5a5085a3ab92917e6ec22491789b42ab79150f
Sebastien Church <schurch@groupeproaction.com>
2019-10-21 07:08:08 -0400

    Reach 1

--- FileReader.py
+++ FileReader.py
@@ -2,5 +2,13 @@ import numpy
 
 data = numpy.genfromtxt(fname="housing.data")
 
+################# Advanced
 for line in data:
     print(*line)
+
+################# Reach 1
+for line in data:
+    list = line.tolist()
+    strList = [str(i) for i in list]
+    print(strList)
+
